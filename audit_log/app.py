ffe0237f0cb192e4491729c9fb621367f3311281
Luke <svs02po@gmail.com>
2022-03-29 23:41:38 -0700

    lab 11 mid

--- audit_log/app.py
+++ audit_log/app.py
@@ -12,6 +12,7 @@ from threading import Thread
 import json
 from base import Base
 from flask_cors import CORS, cross_origin
+import os
 
 if "TARGET_ENV" in os.environ and os.environ["TARGET_ENV"] == "test":
     print("In Test Environment")
