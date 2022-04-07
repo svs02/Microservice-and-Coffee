d5b8cca2667ccc3c82a8d10294969ce5ea2d4c94
Luke <svs02po@gmail.com>
2022-04-07 01:48:40 -0700

    change dashboard health.js style

--- health_check/app.py
+++ health_check/app.py
@@ -133,7 +133,7 @@ app = connexion.FlaskApp(__name__, specification_dir='')
 if "TARGET_ENV" not in os.environ or os.environ["TARGET_ENV"] != "test":
     CORS(app.app)
     app.app.config['CORS_HEADERS'] = 'Content-Type'
-app.add_api('openapi.yaml', base_path="/health_check", strict_validation=True, validate_responses=True)
+app.add_api('openapi.yaml', base_path="/health", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
     check_data()
